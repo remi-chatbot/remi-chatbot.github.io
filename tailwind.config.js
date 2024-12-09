d0af4119080cfbd4e82055735cd2472271b3ffad
longhornyash <132172656+longhornyash@users.noreply.github.com>
2024-12-10 01:31:57 +0530

    package changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path based on your project structure
-    "./public/index.html",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
