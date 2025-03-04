bf917eacc5bb7e1b85dbb4c084060c11a2d3d93e
longhornyash <132172656+longhornyash@users.noreply.github.com>
2025-03-04 22:35:33 +0530

    firebase setup

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBoW8AOs7lG8E9gbSQldPp-9wsFhBqwJQs",
-    authDomain: "remi-e0b0f.firebaseapp.com",
-    projectId: "remi-e0b0f",
-    storageBucket: "remi-e0b0f.firebasestorage.app",
-    messagingSenderId: "172874904191",
-    appId: "1:172874904191:web:8210a34a23fa292ab2054a",
-    measurementId: "G-DJE9ZCNP38"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
